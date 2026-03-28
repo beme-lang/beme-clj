1619540ee9ae7d7aa2020ee84b7f02bd6b93f935
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2026-03-28 20:25:48 +0200

    Add Clojars publish infrastructure (build.clj + :build alias)

---
+++ build.clj
@@ -0,0 +1,39 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
+
+(def lib 'io.github.beme-lang/beme-clj)
+(def version "0.4.0-alpha")
+(def class-dir "target/classes")
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (clean nil)
+  (let [basis @basis]
+    (b/write-pom {:class-dir class-dir
+                  :lib lib
+                  :version version
+                  :basis basis
+                  :src-dirs ["src"]
+                  :scm {:url "https://github.com/beme-lang/beme-clj"
+                        :connection "scm:git:git://github.com/beme-lang/beme-clj.git"
+                        :developerConnection "scm:git:ssh://git@github.com/beme-lang/beme-clj.git"
+                        :tag (str "v" version)}
+                  :pom-data [[:licenses
+                              [:license
+                               [:name "MIT License"]
+                               [:url "https://opensource.org/licenses/MIT"]]]]})
+    (b/copy-dir {:src-dirs ["src"]
+                 :target-dir class-dir})
+    (b/jar {:class-dir class-dir
+            :jar-file jar-file})))
+
+(defn deploy [_]
+  (jar nil)
+  (dd/deploy {:installer :remote
+              :artifact (b/resolve-path jar-file)
+              :pom-file (b/pom-path {:lib lib :class-dir class-dir})}))
